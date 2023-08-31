42f391a7c29eed0b7dedd5dd4e5e1229ac381489
Masante <108529990+sem1312@users.noreply.github.com>
2023-08-31 15:30:07 -0600

    escalonado

--- desafiosJS/desafio2.js
+++ desafiosJS/desafio2.js
@@ -1,13 +1,26 @@
-let N = 14;
-let res = 0;
-
-for(let i=10; i<=N; i++){
-    let str = i.toString()
-    for(let j=0; j<str.length; j++){
-        if(str[j] < str[j+1]){
-            res++;
-        }
+function isEscalonado(n){
+  //separa el numero en un arr de cada digito separado
+  let numArr = String(n).split('').map(Number);
+  for (let j = 1; j < numArr.length; j++) {
+      if (numArr[j] <= numArr[j - 1]) {
+        return false;
+      }
     }
+  return true;
 }
 
-console.log(res);
+function escalonados(N) {
+  let cont = 0;
+  let test = [];
+  
+  for (let i = 10; i <= N; i++) {
+    if (isEscalonado(i)) {
+      cont++;
+      test.push(i);
+    }
+  }
+  console.log(test)
+  return cont;
+}
+
+console.log(escalonados(14));
